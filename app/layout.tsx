3748e34716cc6522a24c7d0542ea99571f8346aa
Miriampally Venkata Sugunadithya <venkatasugunadithya@gmail.com>
2026-06-07 11:14:27 +0530

    made changes to preload the vedio to avoid lag

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={ttInterphasesMono.variable}>
+      <head>
+        <link rel="preload" href="/videos/page_1.mp4" as="video" type="video/mp4" />
+        <link rel="preload" href="/videos/page_2.mp4" as="video" type="video/mp4" />
+      </head>
       <body style={{ margin: 0, overflow: 'hidden', height: '100dvh' }}>
         {children}
       </body>
